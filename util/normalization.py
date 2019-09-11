ab8adb1700fb1dc1979a46b746a7280be1671fe8
Jon Shamir <jon.shamir@gmail.com>
2019-09-11 11:54:08 +0300

    updates

---
+++ util/normalization.py
@@ -0,0 +1,23 @@
+import torch
+from models.vgg_extractor import get_VGG_features
+
+def get_normalization_data(dataset):
+    mean = 0.
+    std = 0.
+    nb_samples = 0.
+    for _, data in enumerate(dataset):
+        data = data['A']
+        batch_samples = data.size(0)
+        data = get_VGG_features(data).detach()
+        data = data.view(batch_samples, data.size(1), -1)
+        mean += data.mean(2).sum(0)
+        std += data.std(2).sum(0)
+        nb_samples += batch_samples
+
+    mean /= nb_samples
+    std /= nb_samples
+
+    mean = mean[None,:,None,None]
+    std = std[None,:,None,None]
+
+    return mean, std
