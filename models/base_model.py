dece5f94f7ac7a8f866132e24d644fb46b0dd88a
Jon Shamir <jon.shamir@gmail.com>
2019-09-25 15:09:14 +0300

    improvements

--- models/base_model.py
+++ models/base_model.py
@@ -141,10 +141,11 @@ class BaseModel(ABC):
     def get_current_visuals_old(self):
         """Return visualization images. train.py will display these images with visdom, and save the images to a HTML"""
         visual_ret = OrderedDict()
-        print (self.visual_names)
         for name in self.visual_names:
             if isinstance(name, str):
                 visual_ret[name] = getattr(self, name)
+        print (len(visual_ret))
+
         return visual_ret
 
     def get_current_losses(self):
