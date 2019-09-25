243c9dd2db8b27de2f5b166cb8ac2fb0f11ddc04
Jon Shamir <jon.shamir@gmail.com>
2019-09-25 15:02:11 +0300

    improvements

--- test.py
+++ test.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
         model.test()           # run inference
         visuals = model.get_current_visuals_old()  # get image results
         img_path = model.get_image_paths()     # get image paths
-        if i % 5 == 0:  # save images to an HTML file
-            print('processing (%04d)-th image... %s' % (i, img_path))
+        if i % 5 == 0: print('processing (%04d)-th image... %s' % (i, img_path))
+        print(visuals.shape)
         save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
     webpage.save()  # save the HTML
