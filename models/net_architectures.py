c2c632f3aa5d4b830ab4a89fb2c250f7ec90ca3d
Jon Shamir <jon.shamir@gmail.com>
2019-10-16 12:51:07 +0300

    fixes

--- models/net_architectures.py
+++ models/net_architectures.py
@@ -31,6 +31,7 @@ class VGGInverterG(nn.Module):
 
         for _ in range(layer-2):
             num_feats /= 2
+            print (num_feats)
 
             model += [
                 nn.ConvTranspose2d(2 * num_feats, num_feats, 4, stride=2, padding=1, bias=False),
