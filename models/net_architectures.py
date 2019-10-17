a2d98061845f72ca518bdef042bb28df04f6a155
Jon Shamir <jon.shamir@gmail.com>
2019-10-17 14:16:55 +0300

    fixes

--- models/net_architectures.py
+++ models/net_architectures.py
@@ -221,7 +221,7 @@ class DeepDiscriminator(nn.Module):
     def __init__(self, layer=5, ndf=128):
         super(DeepDiscriminator, self).__init__()
         gen_ch = VGG_NUM_CHANNELS[layer]
-        num_layers = 10
+        num_layers = 8
         num_strided_layers = 7 - layer
 
         model = []
